414dc2c9c62fb0978aac8610818f1fbc2f3a8622
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-03-30 21:54:37 +0530

    Interface

---
+++ Functionalnterface/src/interfaceUtil/CustomList.java
@@ -0,0 +1,21 @@
+package interfaceUtil;
+
+import java.util.function.Consumer;
+
+public interface CustomList {
+	
+	void add(int item);
+	int size();
+	int get(int index);
+	
+	default void forEach(Consumer<Integer> consumer) {
+		
+		for(int i=0;i<size();i++)
+		{
+			consumer.accept(get(i));
+		}
+	}
+	
+	
+
+}
